6621aa8877f9ef0492241ca6aad8b7f5fe05726a
Mayank Sinha <sinha.mayank900@gmail.com>
2024-07-11 09:03:27 +0530

    Q:853 done.

--- 0853_car_fleet.py
+++ 0853_car_fleet.py
@@ -2,7 +2,7 @@ from typing import List
 
 # Python: Recursion?
 class Solution:
-    def recursive_fleet(fleet):
+    def recursive_fleet(self, fleet, target, count):
         fleet1 = []
         j=0
         for i in range(len(fleet)):
@@ -15,27 +15,41 @@ class Solution:
                 else:
                     fleet1.append([fleet[i][0]+fleet[i][1], fleet[i][1]])
                     j+=1
-        return fleet1
+
+        if fleet[0][0] >= target:
+            count+=1
+
+        count =self.recursive_fleet(fleet, target, count)
+
+        
 
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         fleet0 = [[p,s] for p, s in zip(position, speed)]
         fleet0.sort(reverse=True)
         print(fleet0)
+        print(self.recursive_fleet(fleet0, target, 0))
         
 
-# Pyhton: Other method?
+# Pyhton: Other method? Pushing slower times into a stack and calculating len.
 class Solution2:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        ...
+        fleet0 = [(p,s) for p, s in zip(position, speed)]
+        fleet0.sort(reverse=True)
+        all_times = [(target-fleet0[0][0])/fleet0[0][1]]
+        fleet0.pop(0)
+        for fleet in fleet0:
+            time_taken = (target-fleet[0]) / fleet[1]
+            if time_taken > all_times[-1]:
+                all_times.append(time_taken)
         
-
+        return len(all_times)
 
 
 target = 12
 position = [10,8,0,5,3]
 speed = [2,4,1,1,3]
 
-s = Solution()
+s = Solution2()
 s.carFleet(target=target, position=position, speed=speed)
 
 
@@ -44,7 +58,7 @@ def test_solution():
     position = [10,8,0,5,3]
     speed = [2,4,1,1,3]
 
-    s = Solution()
+    s = Solution2()
     assert s.carFleet(target=target, position=position, speed=speed) == 3
     
 def test_solution2():
@@ -52,6 +66,6 @@ def test_solution2():
     position = [0,2,4]
     speed = [4,2,1]
 
-    s = Solution()
+    s = Solution2()
     assert s.carFleet(target=target, position=position, speed=speed) == 1
     
