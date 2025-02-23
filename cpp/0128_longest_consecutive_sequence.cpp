f0f4ea6ab4a070725f97a7ebcfeee4fa1733d3d0
Mayank Sinha <sinha.mayank900@gmail.com>
2025-02-23 16:18:50 +0530

    Recommit after removing cache to remove .idea

--- cpp/0128_longest_consecutive_sequence.cpp
+++ cpp/0128_longest_consecutive_sequence.cpp
@@ -11,23 +11,22 @@ class Solution {
     int max_length{};
 
     unordered_map<int, int> hash_map{};
-    // hash_map.insert({1, 1});
 
     for (auto& num : nums) {
       if (hash_map.find(num) != hash_map.end()) {
         continue;
       }
       hash_map[num] = 0;
-      auto num_left =
+      const auto num_left =
           (hash_map.find(num - 1) != hash_map.end()) ? hash_map[num - 1] : 0;
-      auto num_right =
+      const auto num_right =
           (hash_map.find(num + 1) != hash_map.end()) ? hash_map[num + 1] : 0;
       int count = num_right + num_left + 1;
       hash_map[num - num_left] = count;
       hash_map[num + num_right] = count;
       std::cout << num << " ";
-      for (auto& i : hash_map) {
-        std::cout << "{" << i.first << "," << i.second << "}, ";
+      for (const auto &[key, value] : hash_map) {
+        std::cout << "{" << key << "," << value << "}, ";
       }
       std ::cout << std::endl;
       max_length = max(max_length, count);
@@ -42,7 +41,6 @@ int main() {
   std::vector<int> nums = {4,  0,  -4, -2, 2, 5, 2,  0, -8, -8, -8,
                            -8, -1, 7,  4,  5, 5, -4, 6, 6,  -3};
 
-  auto out = s.longestConsecutive(nums);
-  std::cout << out;
-  // assert(out == 4);
+  const auto out = s.longestConsecutive(nums);
+  assert(out == 5);
 }
